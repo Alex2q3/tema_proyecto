7e5b1a1cd54cd848fff1b8fc4d62e5dde41dec54
Alex2q3 <77866128+Alex2q3@users.noreply.github.com>
2022-01-18 23:17:52 -0500

    commit nuevos datos conexion

--- conexiones/datos.php
+++ conexiones/datos.php
@@ -1,9 +1,12 @@
 <?php
 //la conexión depende de cada ambiente configurado en el servidor
-$host="localhost";
-$usuario="adproject";
-$clave="Telecom321%";
+$host="127.0.0.1";
+$usuario="root";
+$clave="";
 $base="pry_honeypot";
 $conexion=mysqli_connect("$host","usuario","clave") or die();
 $base=mysqli_select_db($conexion, $base);
+
+echo "Conexion lista";
+
 ?>
